4b8c2b621f872e408445b40fb245180fc85a6518
Ben Howdle <benhowdle89@gmail.com>
2017-09-20 09:34:08 +0100

    show images

--- src/components/tweet.vue
+++ src/components/tweet.vue
@@ -1,6 +1,8 @@
 <template lang="pug">
   .tweet
-    pre(v-html="$options.filters.linkify(tweet.text)")
+    pre(v-html="$options.filters.linkify(strippedTweet)")
+    .images(v-if="media")
+      img(v-for="m in media", :src="imageUrl(m)")
     p @{{ tweet.user.screen_name }}
 </template>
 
@@ -11,7 +13,22 @@ export default {
     tweet: Object
   },
   filters: {
-    linkify: html => linkifyHtml(html, { defaultProtocol: 'https' })
+    linkify: (html = '') => linkifyHtml(html, { defaultProtocol: 'https' })
+  },
+  methods: {
+    imageUrl ({ media_url }) { return `${media_url}:small` }
+  },
+  computed: {
+    media () { return this.tweet.extended_entities && this.tweet.extended_entities.media },
+    strippedTweet () {
+      let text = this.tweet.text
+      if (!this.media) return text
+      this.media.forEach(m => {
+        const [start, end] = m.indices
+        text = text.slice(0, start)
+      })
+      return text
+    }
   }
 }
 </script>
