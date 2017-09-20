751794c436fecb8763cbf7e9d341e0585cde2636
Ben Howdle <benhowdle89@gmail.com>
2017-09-20 07:18:23 +0100

    fetch within window

--- src/components/timeline.vue
+++ src/components/timeline.vue
@@ -1,5 +1,6 @@
 <template lang="pug">
   .timeline
+    button(v-if="canFetchTweets", @click="fetchTweets") Fetch tweets
     .tweet(v-for="tweet in tweets")
       p {{ tweet.text }}
       p @{{ tweet.user.screen_name }}
@@ -7,12 +8,14 @@
 
 <script>
 import { mapActions, mapState } from 'vuex'
+import { haveFetchedInWindow } from './../helpers'
 export default {
   methods: {
     ...mapActions(['fetchTweets'])
   },
   computed: {
-    ...mapState(['tweets'])
+    ...mapState(['tweets', 'lastFetched']),
+    canFetchTweets () { return !haveFetchedInWindow(this.lastFetched) }
   },
   mounted () {
     !this.tweets.length && this.fetchTweets()
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,3 +1,13 @@
+import differenceInMinutes from 'date-fns/difference_in_minutes'
+
 const TWITTER_BASE_URL = 'https://twitter.com/'
+const FETCH_WINDOW = 15
 
 export const twitterOAuthURL = token => `${TWITTER_BASE_URL}oauth/authenticate?oauth_token=${token}`
+export const haveFetchedInWindow = lastFetched => {
+  const fetchedAgo = differenceInMinutes(
+    new Date(),
+    lastFetched
+  )
+  return fetchedAgo < FETCH_WINDOW
+}
