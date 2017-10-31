f3d40e65d8f0da345844864d9291af7f98613690
Scott Riley <hugs@scottriley.im>
2017-10-31 20:35:19 +0000

    Loads in OG metadata for external links (when there’s at least a title to show)

--- src/components/tweet.vue
+++ src/components/tweet.vue
@@ -15,6 +15,13 @@
         youtube(:video-id="videoId", v-for="videoId in videoIds", :key="videoId")
       .videos.p2.mt2(v-if="videos")
         v-video(:video="videos")
+      a.externalLink(:href="externalLink.link" target="_blank" v-if="showExternal")
+        .extImg(v-if="externalLink.image")
+          img(:src="externalLink.image")
+        div.extContent
+          h3.extTitle {{ externalLink.title }}
+          p.extDescription {{ externalLink.description }}
+          p.extSite {{ externalLink.site }}
 </template>
 
 <script>
@@ -38,6 +45,18 @@ export default {
   },
   methods: {
     imageUrl ({ media_url }) { return `${media_url}:small` }
+  },
+  data() {
+    return { 
+      externalLink: {
+        image: '',
+        title: '',
+        description: '',
+        site: '',
+        link: '',
+      },
+      showExternal: false, 
+    }
   },
   computed: {
     videoIds () { return this.urls && this.urls.map(({ expanded_url }) => {
@@ -73,6 +92,41 @@ export default {
       })
       return text
     }
+  },
+  async mounted () {
+    // const html = await fetch('https://cors-anywhere.herokuapp.com/http://www.imdb.com/title/tt1375666/')
+    if(this.urls[0]) {
+      const yql = `select * from htmlstring where url='${this.urls[0].expanded_url}' AND xpath='/html/head/meta'`
+      const queryUrl = `http://query.yahooapis.com/v1/public/yql?q=${encodeURIComponent(yql)}&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys`
+      await fetch(queryUrl)
+        .then(data => {
+          data.json().then( meta => {
+              let metaString = `<html><head>${meta.query.results.result}</head><body></body>`
+              let parser = new DOMParser()
+              let htmlDoc = parser.parseFromString(metaString, "text/xml")
+              let metas = htmlDoc.getElementsByTagName('meta')
+              this.externalLink.site = this.urls[0].expanded_url
+              this.externalLink.link = this.urls[0].expanded_url
+              for(let metaTag of metas) {
+                if(metaTag.getAttribute("property") == "og:image") {
+                  this.externalLink.image = metaTag.getAttribute("content")
+                }
+                if(metaTag.getAttribute("property") == "og:title") {
+                  this.externalLink.title = metaTag.getAttribute("content").substring(0,60) + '…'
+                }
+                if(metaTag.getAttribute("property") == "og:description") {
+                  this.externalLink.description = metaTag.getAttribute("content").substring(0,60) + '…'
+                }
+                if(metaTag.getAttribute("property") == "og:site_name") {
+                  this.externalLink.site = metaTag.getAttribute("content").toLowerCase()
+                }
+              }
+              if(this.externalLink.title) {
+                this.showExternal = true
+              }
+          })
+        })
+    } 
   },
   components: { VVideo, ProfilePicture }
 }
@@ -117,7 +171,7 @@ export default {
     font-weight: 300
 .text
   font-weight: 300
-  font-size: 1.1rem
+  font-size: 1.14rem
   overflow: hidden
   text-overflow: ellipsis
   a
@@ -145,7 +199,58 @@ export default {
   left: 0
   width: 100%
   height: 100%
-  
+
+.externalLink
+  width: 100%
+  height: 200px
+  background: rgba(40,41,46,.03)
+  border-radius: 4px
+  position: relative
+  padding-left: calc(200px + 2em)
+  padding-top: 1.2em
+  padding-right: 1em
+  display: block
+  text-decoration: none
+
+.extImg
+  width: 200px
+  height: 200px
+  overflow: hidden
+  position: absolute
+  top: 0
+  left: 0
+
+.extImg img
+  position: absolute
+  top: 0
+  left: 0
+  height: 100%
+  min-width: 100%
+  border-top-left-radius: 4px
+  border-bottom-left-radius: 4px
+
+.extTitle
+  font-family: 'Rubik', sans-serif
+  color: #292929
+  font-weight: 500
+  font-size: 1rem
+  line-height: 1.1
+
+.extSite
+  color: #7994E5
+  font-family: 'Rubik', sans-serif
+  font-weight: 100
+  position: absolute
+  bottom: 1em
+  font-size: .84rem
+
+.extDescription
+  font-style: italic
+  font-size: 1.1rem
+  color: #7D7D7D
+  line-height: 1.2
+  padding-top: 1rem
+
 </style>
 
 <style lang="sass">
