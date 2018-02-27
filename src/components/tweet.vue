d2094da6047c3dab98b2d0f42e19b872ad33a0a2
Ben Howdle <benhowdle89@gmail.com>
2018-02-27 10:56:20 +0000

    autoplay off and permalink

--- src/components/tweet.vue
+++ src/components/tweet.vue
@@ -2,7 +2,9 @@
   .tweet.pt2.pb3.mb3.flex
     .meta.pb0
       profile-picture.self-center(:user="tweet.user", size="large")
-      p.ago.right-align.h5.italic {{ ago }}
+      p.ago.right-align.h5.italic
+        a(v-if="tweet.id_str", :href="tweetURL") {{ ago }}
+        span(v-else) {{ ago }}
     .content.pl0.pb0
       p.person.mb0
         span.name {{ tweet.user.name }}
@@ -19,65 +21,98 @@
 </template>
 
 <script>
-import linkifyHtml from 'linkifyjs/html'
-import { getIdFromURL } from 'vue-youtube-embed'
-import { ago } from './../helpers'
-import ExternalLink from './external-link'
-import ProfilePicture from './profile-picture'
-import VVideo from './video'
+import linkifyHtml from "linkifyjs/html";
+import { getIdFromURL } from "vue-youtube-embed";
+import { ago } from "./../helpers";
+import ExternalLink from "./external-link";
+import ProfilePicture from "./profile-picture";
+import VVideo from "./video";
 export default {
   props: {
     tweet: Object
   },
   filters: {
-    linkify: (html = '') => linkifyHtml(html, { defaultProtocol: 'https' }),
-    tweetify: (html = '') => {
-      let tweet = html
-      tweet = tweet.replace(/(^|\s)@(\w+)/g, '$1<a href="http://twitter.com/$2" target="_blank">@$2</a>');
-      return tweet.replace(/(^|\s)#(\w+)/g, '$1<a href="http://twitter.com/search?q=%23$2" target="_blank">#$2</a>')
+    linkify: (html = "") => linkifyHtml(html, { defaultProtocol: "https" }),
+    tweetify: (html = "") => {
+      let tweet = html;
+      tweet = tweet.replace(
+        /(^|\s)@(\w+)/g,
+        '$1<a href="http://twitter.com/$2" target="_blank">@$2</a>'
+      );
+      return tweet.replace(
+        /(^|\s)#(\w+)/g,
+        '$1<a href="http://twitter.com/search?q=%23$2" target="_blank">#$2</a>'
+      );
     },
-    nl2br: (html = '') => html.replace(/(?:\r\n|\r|\n)/g, '<br />')
+    nl2br: (html = "") => html.replace(/(?:\r\n|\r|\n)/g, "<br />")
   },
   methods: {
-    imageUrl ({ media_url }) { return `${media_url.replace(/^http:\/\//i, 'https://')}:small` }
+    imageUrl({ media_url }) {
+      return `${media_url.replace(/^http:\/\//i, "https://")}:small`;
+    }
   },
   computed: {
-    videoIds () { return this.urls && this.urls.map(({ expanded_url }) => {
-      return /youtube/i.test(expanded_url) && getIdFromURL(expanded_url)
-    }).filter(Boolean)},
-    videos () {
-      if (!this.media) return false
-      const videos = this.media.find(m => m.video_info)
-      if (!videos) return false
-      const { video_info: { variants }, media_url_https } = videos
-      const { url, content_type } = variants.find(v => v.url)
+    videoIds() {
+      return (
+        this.urls &&
+        this.urls
+          .map(({ expanded_url }) => {
+            return /youtube/i.test(expanded_url) && getIdFromURL(expanded_url);
+          })
+          .filter(Boolean)
+      );
+    },
+    videos() {
+      if (!this.media) return false;
+      const videos = this.media.find(m => m.video_info);
+      if (!videos) return false;
+      const { video_info: { variants }, media_url_https } = videos;
+      const { url, content_type } = variants.find(v => v.url);
       return {
         image: media_url_https,
         source: url,
         type: content_type
-      }
+      };
     },
-    ago () { return ago(this.tweet.created_at) },
-    media () { return this.tweet.extended_entities && this.tweet.extended_entities.media },
-    images () { return this.media && this.tweet.extended_entities.media.filter(m => m.type !== 'animated_gif') },
-    urls () { return this.tweet.entities && this.tweet.entities.urls },
-    strippedTweet () {
-      let text = this.tweet.text
-      this.media && this.media.forEach(m => {
-        const [start, end] = m.indices
-        text = text.slice(0, start)
-      })
-      if (!this.urls) return text
+    ago() {
+      return ago(this.tweet.created_at);
+    },
+    media() {
+      return this.tweet.extended_entities && this.tweet.extended_entities.media;
+    },
+    images() {
+      return (
+        this.media &&
+        this.tweet.extended_entities.media.filter(
+          m => m.type !== "animated_gif"
+        )
+      );
+    },
+    urls() {
+      return this.tweet.entities && this.tweet.entities.urls;
+    },
+    tweetURL() {
+      const { id_str, user: { screen_name } } = this.tweet;
+      return `https://twitter.com/${screen_name}/status/${id_str}`;
+    },
+    strippedTweet() {
+      let text = this.tweet.text;
+      this.media &&
+        this.media.forEach(m => {
+          const [start, end] = m.indices;
+          text = text.slice(0, start);
+        });
+      if (!this.urls) return text;
       this.urls.forEach(urlObj => {
-        const { expanded_url, url } = urlObj
-        const re = new RegExp(url)
-        text = text.replace(re, expanded_url)
-      })
-      return text
+        const { expanded_url, url } = urlObj;
+        const re = new RegExp(url);
+        text = text.replace(re, expanded_url);
+      });
+      return text;
     }
   },
   components: { VVideo, ProfilePicture, ExternalLink }
-}
+};
 </script>
 
 <style lang="sass">
--- src/components/video.vue
+++ src/components/video.vue
@@ -1,17 +1,17 @@
 <template lang="pug">
 .video
-  video(autoplay, loop, muted, :poster="video.image")
+  video(controls, loop, muted, :poster="video.image")
     source(:src="video.source", :type="video.type")
 </template>
 
 <script>
 export default {
- props: {
-   video: {
-     type: Object
-   }
- } 
-}
+  props: {
+    video: {
+      type: Object
+    }
+  }
+};
 </script>
 
 <style lang="sass">
